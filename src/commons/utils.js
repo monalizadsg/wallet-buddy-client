feec5fba1eab9ce70f45159cc742f71e76d6c92f
Mona Dasig <monalizadasig@gmail.com>
2024-02-21 07:58:44 -0800

    Add reusable custom toast

--- src/commons/utils.js
+++ src/commons/utils.js
@@ -1,3 +1,22 @@
+import { useToast } from "@chakra-ui/react";
+
+export const CustomToast = () => {
+  const toast = useToast();
+  // types: "success", "info", "warning", "error"
+
+  const addToast = (newRes) => {
+    toast({
+      title: newRes.title,
+      description: newRes.message,
+      status: newRes.type,
+      isClosable: true,
+      duration: 3000,
+    });
+  };
+
+  return { addToast };
+};
+
 export const getUserId = () => {
   return localStorage.getItem("id");
 };
